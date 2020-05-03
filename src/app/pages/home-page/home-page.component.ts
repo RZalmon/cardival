7fc87178b290240f3c7dbb83ee0a67115cc29be7
Rami Davidov <rami.davidov@gmail.com>
2020-05-03 14:56:18 +0300

    join / host home-page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UtilService } from 'src/app/services/util.service';
+
 
 @Component({
   selector: 'home-page',
@@ -6,16 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  pageState = 0
+  isJoinShown = false
+  roomId = ''
 
-  constructor() { }
+  constructor(private utilService: UtilService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  changePageState(num: number): void {
-    this.pageState = num
-    console.log('this.pageState:', this.pageState)
+  onSubmit(): void {
+    if (this.roomId) this.joinRoom(this.roomId)
   }
 
+  joinRoom(id: string): void {
+    this.router.navigate([`/room/${id}`])
+  }
+
+  hostRoom(): void {
+    const id = this.utilService.setId()
+    // Save user is host for the this id to localstorage
+    this.joinRoom(id)
+  }
+
+  toggleJoinRoom(): void {
+    this.isJoinShown = !this.isJoinShown
+  }
 }
