e13a2bfd2eab2d169f3f418bba0be7b7f7db9828
Your NameTamir <tamirn7@gmail.com>
2020-05-03 12:19:43 +0300

    shorthand for cards

--- src/app/cmps/card-preview/card-preview.component.ts
+++ src/app/cmps/card-preview/card-preview.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Card } from 'src/app/models/card.model';
-// import ass from '../../../assets/img/clubs.svg'
 
 @Component({
   selector: 'card-preview',
@@ -11,28 +10,11 @@ export class CardPreviewComponent implements OnInit {
   @Input() card: Card;
 
 get suit(){
-  let {suit} = this.card;
-switch (suit) {
-  case 'hearts':
-   return '../../../assets/img/hearts.svg'
-   break;
-   case 'diamonds':
-    return '../../../assets/img/diamonds.svg'
-    break;
-    case 'spades':
-      return '../../../assets/img/spades.svg'
-      break;
-         case 'clubs':
-          return '../../../assets/img/clubs.svg'
-          break;
-  default:
-}
+  return `./assets/img/${this.card.suit}.svg`;
 }
 
   constructor() { }
-
   ngOnInit(): void {
-    console.log(this.card);
     
   }
 
