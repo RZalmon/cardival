c5259e41e6880fb437f7a78743356f14d0587769
Your NameTamir <tamirn7@gmail.com>
2020-05-03 12:14:29 +0300

    first card fix

--- src/app/cmps/card-preview/card-preview.component.ts
+++ src/app/cmps/card-preview/card-preview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Card } from 'src/app/models/card.model';
+// import ass from '../../../assets/img/clubs.svg'
 
 @Component({
   selector: 'card-preview',
@@ -9,10 +10,30 @@ import { Card } from 'src/app/models/card.model';
 export class CardPreviewComponent implements OnInit {
   @Input() card: Card;
 
+get suit(){
+  let {suit} = this.card;
+switch (suit) {
+  case 'hearts':
+   return '../../../assets/img/hearts.svg'
+   break;
+   case 'diamonds':
+    return '../../../assets/img/diamonds.svg'
+    break;
+    case 'spades':
+      return '../../../assets/img/spades.svg'
+      break;
+         case 'clubs':
+          return '../../../assets/img/clubs.svg'
+          break;
+  default:
+}
+}
+
   constructor() { }
 
   ngOnInit(): void {
-    console.log('CARDD', this.card)
+    console.log(this.card);
+    
   }
 
 }
