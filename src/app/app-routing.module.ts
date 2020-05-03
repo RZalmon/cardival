675aa76614a532eb4a99caec563cf4cedd69b0ff
RZalmon <ranzlmn@gmail.com>
2020-05-03 12:56:28 +0300

    home crated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { RoomPageComponent } from './pages/room-page/room-page.component'
 
 const routes: Routes = [];
 
