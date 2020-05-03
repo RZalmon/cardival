81551db4028f9cada8c446c80821cc42b1a01e26
RZalmon <ranzlmn@gmail.com>
2020-05-03 13:43:59 +0300

    card name and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component'
 import { RoomPageComponent } from './pages/room-page/room-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'room/:id', component: RoomPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
