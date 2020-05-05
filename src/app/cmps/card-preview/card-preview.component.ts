88126d311e1d8bde79265e9131bb60002b95098d
RZalmon <ranzlmn@gmail.com>
2020-05-05 13:56:45 +0300

    trying to fix card drag

--- src/app/cmps/card-preview/card-preview.component.ts
+++ src/app/cmps/card-preview/card-preview.component.ts
@@ -11,66 +11,73 @@ import { Card } from 'src/app/models/card.model';
 export class CardPreviewComponent implements OnInit {
   @Input() card: Card;
   @Input() cards: Card[];
-  @Input() zIndex:number;
+  @Input() zIndex: number;
   @Output() onUpdateZIndex = new EventEmitter<any>();
 
 
-  currCard:Card = this.card
-  mouseX:number = null;
-  mouseY:number = null;
-  dragPosition = {x: 0, y: 0};
+  currCard: Card = this.card
+  mouseX: number = null;
+  mouseY: number = null;
+  dragPosition = { x: 0, y: 0 };
 
-get suit(){
-  return `./assets/img/${this.card.suit}.svg`;
-}
+  get suit() {
+    return `./assets/img/${this.card.suit}.svg`;
+  }
 
-onDragStart(card, ev) {
-<<<<<<< HEAD
-  console.log(ev); 
-  if (ev.target.localName === 'h2' || ev.target.localName === 'img') ev.target.offsetParent.style.zIndex = this.zIndex + '' //PLASTER
-  else ev.target.style.zIndex = this.zIndex + ''
-=======
-  ev.target.offsetParent.style.zIndex = this.zIndex + ''
->>>>>>> ba24cab7c93cefdcbb874571292cd9ad917357a3
-  this.onUpdateZIndex.emit()
-  window.addEventListener('mousemove', this.onMouseMove)
-}
+  onDragStart(card, ev) {
+    if (ev.target.localName === 'h2' || ev.target.localName === 'img') ev.target.offsetParent.style.zIndex = this.zIndex + '' //PLASTER
+    else ev.target.style.zIndex = this.zIndex + ''
+    this.onUpdateZIndex.emit()
+    window.addEventListener('mousemove', this.onMouseMove)
+  }
 
-onDragOver(ev){   
-  this.currCard = null
+  onDragOver(ev) {
+    console.log('invoked!');
 
-  window.removeEventListener('mousemove', ()=>{
-    console.log('removed!');
-    this.socketService.off('card move',()=>{
+    // this.currCard = null
+    window.removeEventListener('mousemove', this.onMouseMove)
+    this.socketService.off('card move', () => {
       console.log('card moveeee');
-      
     })
-  })  
-  
-}
+
+  }
 
 
-onMouseMove = (ev) => {    
-  this.mouseX = ev.clientX
-  this.mouseY = ev.clientY
-  this.socketService.emit('card move', {card:this.card,locX:this.mouseX, locY:this.mouseY})
-}
+  onMouseMove = (ev) => {
+    this.mouseX = ev.offsetX
+    this.mouseY = ev.offsetY
+    this.socketService.emit('card move', { card: this.card, locX: this.mouseX, locY: this.mouseY })
+  }
 
 
 
   constructor(private socketService: SocketService,
-    ) { }
-   ngOnInit(): void {
-    
-    this.socketService.on('card moved', ({card,locX,locY}) => {   
-      this.currCard =  this.cards.find(currCard=>{ 
+  ) { }
+  ngOnInit(): void {
+
+    this.socketService.on('card moved', ({ card, locX, locY }) => {
+      this.currCard = this.cards.find(currCard => {
         return currCard._id === card._id
       })
       //  this.dragPosition = this.currCard.dragPosition 
-      if(this.currCard){
-        this.currCard.dragPosition = {x:locX,y:locY}
+      if (this.currCard) {
+        this.currCard.dragPosition = { x: locX, y: locY }
       }
-      })
+    })
   }
 
 }
+
+// onDragOver(ev) {
+//   console.log('invoked!');
+
+//   this.currCard = null
+//   window.removeEventListener('mousemove', () => {
+//     console.log('removed!');
+//     this.socketService.off('card move', () => {
+//       console.log('card moveeee');
+
+//     })
+//   })
+
+// }
