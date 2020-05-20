45cc26db9b3fb099b4110ab2f3b5df8c5abc1aa1
Your NameTamir <tamirn7@gmail.com>
2020-05-20 14:19:38 +0300

    trying to push

--- src/app/cmps/card-preview/card-preview.component.ts
+++ src/app/cmps/card-preview/card-preview.component.ts
@@ -49,7 +49,7 @@ export class CardPreviewComponent implements OnInit {
       console.log('card moveeee');
     });
   }
-  onMouseMove = (card) => {
+  onMouseMove = (card) => {        
     if (!this.isMoving) return;
     this.mouseX =
       this.cardPreview.nativeElement.getBoundingClientRect().left -
@@ -67,97 +67,19 @@ export class CardPreviewComponent implements OnInit {
       card: card,
       locX: this.mouseX,
       locY: this.mouseY,
+      
     });
   };
   
 
   ngOnInit(): void {
-    this.socketService.on('card moved', ({ card, locX, locY }) => {
+    this.socketService.on('card moved', ({ card, locX, locY}) => {
       this.currCard = this.cards.find((currCard) => {
         return currCard._id === card._id;
-      });
-
-      if (this.currCard) {
-        
+      });      
+      if (this.currCard) {        
         this.currCard.dragPosition = { x: locX - card.top, y: locY - card.left };
       }
     });
   }
-  // ngAfterViewInit(){
-  //   this.card.dragPosition = {
-  //     x:
-  //       this.cardPreview.nativeElement.getBoundingClientRect().left -
-  //       this.boardPos.left,
-  //     y:
-  //       this.cardPreview.nativeElement.getBoundingClientRect().top -
-  //       this.boardPos.top,
-  //   };
-
-  //   console.log(this.card);
-
-  // }
 }
-
-// import { SocketService } from './../../services/socket.service';
-// import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
-// import { Card } from 'src/app/models/card.model';
-// @Component({
-//   selector: 'card-preview',
-//   templateUrl: './card-preview.component.html',
-//   styleUrls: ['./card-preview.component.scss']
-// })
-// export class CardPreviewComponent implements OnInit {
-//   constructor(private socketService: SocketService,
-//   ) { }
-//   @ViewChild('cardPreview') cardPreview;
-//   @Input() card: Card;
-//   @Input() cards: Card[];
-//   @Input() zIndex: number;
-//   @Input() isMoving: boolean
-//   @Output() onUpdateZIndex = new EventEmitter<any>();
-//   @Output() onChangeMoveState = new EventEmitter<any>();
-//   currCard: Card
-//   mouseX: number = null;
-//   mouseY: number = null;
-//   dragPosition = { x: 0, y: 0 };
-//   boardPos = document.querySelector('.board-cmp').getBoundingClientRect()
-//   cardPos = this.cardPreview.nativeElement.getBoundingClientRect()
-//   get suit() {
-//     return `./assets/img/${this.card.suit}.svg`;
-//   }
-//   onDragStart(card, ev) {
-//     this.onChangeMoveState.emit(true)
-//     if (ev.target.localName === 'h2' || ev.target.localName === 'img') ev.target.offsetParent.style.zIndex = this.zIndex + '' //PLASTER
-//     else ev.target.style.zIndex = this.zIndex + ''
-//     this.onUpdateZIndex.emit()
-//     window.addEventListener('mousemove', this.onMouseMove)
-//   }
-//   onDragOver(ev) {
-//     this.onChangeMoveState.emit(false)
-//     this.currCard = null
-//     window.removeEventListener('mousemove', this.onMouseMove, false)
-//     this.socketService.off('card move', () => {
-//       console.log('card moveeee');
-//     })
-//   }
-//   onMouseMove = (ev) => {
-//     console.log('top:', this.cardPos.top - this.boardPos.top);
-//     console.log('right:',  this.cardPos.right - this.boardPos.right);
-//     console.log('bottom:',  this.cardPos.bottom - this.boardPos.bottom);
-//     console.log('left:', this.cardPos.left - this.boardPos.left);
-//     if (!this.isMoving) return
-//     this.mouseX = ev.layerX
-//     this.mouseY = ev.layerY
-//     this.socketService.emit('card move', { card: this.card, locX: this.mouseX, locY: this.mouseY })
-//   }
-//   ngOnInit(): void {
-//     this.socketService.on('card moved', ({ card, locX, locY }) => {
-//       this.currCard = this.cards.find(currCard => {
-//         return currCard._id === card._id
-//       })
-//       if (this.currCard) {
-//         this.currCard.dragPosition = { x: locX - 30, y: locY - 50 }
-//       }
-//     })
-//   }
-// }
